2483268c3eda898a4b893137c4fe7c53f3a4d8c8
pemidi <rashidi.peyman@gmail.com>
2022-02-21 13:43:50 +0330

    add verbosename to fields of profile model

--- account/models.py
+++ account/models.py
@@ -7,12 +7,12 @@ class Profile(models.Model):
     This model represent a profile of a user.
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, verbose_name='first name')
+    last_name = models.CharField(max_length=50, verbose_name='last name')
     email = models.EmailField(max_length=254)
-    phone_number = models.CharField(max_length=20)
-    modified_time = models.DateTimeField(auto_now_add=True)
-    created_time = models.DateTimeField(auto_now=True)
+    phone_number = models.CharField(max_length=20, verbose_name='phone number', unique=True)
+    modified_time = models.DateTimeField(auto_now_add=True, verbose_name='updated time of profile')
+    created_time = models.DateTimeField(auto_now=True, verbose_name='created time of profile')
     
     def __str__(self):
         return self.user.username
