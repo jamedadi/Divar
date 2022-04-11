996def7ed3275d25d420fa8bc2cee2d01ca4dbb8
pemidi <rashidi.peyman@gmail.com>
2022-04-11 06:41:57 +0430

    change position of local_config import

--- divar/settings.py
+++ divar/settings.py
@@ -1,3 +1,4 @@
+from local_config import *
 """
 Django settings for divar project.
 
@@ -15,12 +16,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s!0)&dg92l#rz_j)^@7xw%)_g0ue51#c#hwr(8@w)6!+!ehkt1'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'divar.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-from local_config import *
+
 
 DATABASES = {
 
