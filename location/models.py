b75170a2a43cfd4befff9cc7507c411406427662
pemidi <rashidi.peyman@gmail.com>
2022-04-15 11:17:37 +0430

    fix typo

--- location/models.py
+++ location/models.py
@@ -1,33 +1,31 @@
 from django.db import models
 
 
-
 class Province(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, allow_unicode='True')
-    
+
     def __str__(self):
         return f"{self.name}"
-    
+
     class Meta:
         verbose_name = 'Province'
         verbose_name_plural = 'Provinces'
 
 
-    
 class City(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, allow_unicode='True')
     state = models.ForeignKey(Province, related_name='cities', on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return f"{self.name}"
-    
+
     class Meta:
         verbose_name = "City"
-        verbose_name_plural = "Cities"    
+        verbose_name_plural = "Cities"
 
-    
+
 class District(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, allow_unicode='True')
@@ -35,24 +33,23 @@ class District(models.Model):
 
     def __str__(self):
         return f"{self.name}"
-    
+
     class Meta:
         verbose_name = "District"
         verbose_name_plural = "Districts"
-    
-    
+
 
 class Location(models.Model):
     """
-    This model represent location of published advertisement by User
+    This model represents location of published advertisement by User
     """
     province = models.ForeignKey(Province, on_delete=models.CASCADE)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     district = models.ForeignKey(District, on_delete=models.CASCADE, blank=True, null=True)
-    
+
     def __str__(self):
         return f"{self.city} < {self.province}"
-    
+
     class Meta:
         verbose_name = "Location"
         verbose_name_plural = "Locations"
