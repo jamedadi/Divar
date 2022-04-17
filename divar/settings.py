bfb545bb6248fc7c7f332b5180982dacd5c6fcdc
pemidi <rashidi.peyman@gmail.com>
2022-04-17 08:20:51 +0430

    fix template os.path

--- divar/settings.py
+++ divar/settings.py
@@ -1,3 +1,5 @@
+import os
+
 from local_config import *
 """
 Django settings for divar project.
@@ -58,7 +60,7 @@ ROOT_URLCONF = 'divar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -136,6 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
