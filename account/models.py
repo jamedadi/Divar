608dc83224b4bf4e00b10a0971c053e4932c2d95
pemidi <rashidi.peyman@gmail.com>
2022-04-17 12:22:03 +0430

    change relation type of profile to user

--- account/models.py
+++ account/models.py
@@ -6,13 +6,17 @@ class Profile(models.Model):
     """"
     This model represent a profile of a user.
     """
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50, verbose_name='first name')
     last_name = models.CharField(max_length=50, verbose_name='last name')
     phone_number = models.CharField(max_length=20, verbose_name='phone number', unique=True)
     modified_time = models.DateTimeField(auto_now_add=True, verbose_name='updated time of profile')
     created_time = models.DateTimeField(auto_now=True, verbose_name='created time of profile')
-    
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
     def __str__(self):
         return self.user.username
     
