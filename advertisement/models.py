54a30512d6d5b7dda2ca6de74241262e78400138
pemidi <rashidi.peyman@gmail.com>
2022-04-26 15:59:42 +0430

    add attribute to advertisement app

--- advertisement/models.py
+++ advertisement/models.py
@@ -7,6 +7,10 @@ from location.models import Location
 
 
 class Advertisement(models.Model):
+    """
+    This class represents Advertisement model
+    Each user can one or more advertisement to publish
+    """
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     price = models.PositiveIntegerField(default=0)
@@ -23,9 +27,24 @@ class Advertisement(models.Model):
 
 
 class Image(models.Model):
+    """
+    This class represents Image model
+    Each advertisement has one or many images
+    """
     advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE, related_name='images')
     image_file = models.FileField(
                                     upload_to='images/advertisement',
                                     validators=[FileExtensionValidator(allowed_extensions=('jpg', 'png', 'jpeg'))]
-                                 )
+    )
+
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class AdvAttrValue(models.Model):
+    advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE, related_name='attributes')
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE, related_name='attributes')
+    value = models.CharField(max_length=50)
+
 
