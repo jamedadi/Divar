4862fbcf5006b15e2ea4b97d6de6cad2163c38a4
pemidi <rashidi.peyman@gmail.com>
2022-04-28 18:02:38 +0430

    add help text message to email field in User model

--- account/models.py
+++ account/models.py
@@ -20,7 +20,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     first_name = models.CharField(_('first name'), max_length=150, blank=True)
     last_name = models.CharField(_('last name'), max_length=150, blank=True)
-    email = models.EmailField(_('email address'), blank=True)
+    email = models.EmailField(_('email address'),help_text=_('Enter a valid Email address'), blank=True)
     phone_number = models.CharField(blank=True, max_length=11)
     is_staff = models.BooleanField(
         _('staff status'),
