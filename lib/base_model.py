b55df95be01442bd16290eb5971b4381f931913d
pemidi <rashidi.peyman@gmail.com>
2022-05-01 04:04:14 +0430

    add abstract=True to BaseModel

--- lib/base_model.py
+++ lib/base_model.py
@@ -8,3 +8,6 @@ class BaseModel(models.Model):
     """
     created_time = models.DateTimeField(auto_now_add=True)
     modified_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
