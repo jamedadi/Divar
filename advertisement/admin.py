ee2ac53ded351829cf7c850b1d1e4c1ab990a36c
pemidi <rashidi.peyman@gmail.com>
2022-05-02 02:53:28 +0430

    add TabularInline to Advertisement for images

--- advertisement/admin.py
+++ advertisement/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from .models import Advertisement
+from .models import Advertisement, AdvertisementImage
+
+
+class AdvertisementImageInline(admin.TabularInline):
+    model = AdvertisementImage
 
 
 @admin.register(Advertisement)
 class AdvertisementAdmin(admin.ModelAdmin):
     list_display = ('title', 'category')
+    inlines = [
+        AdvertisementImageInline
+    ]
+    # list_select_related = ('images', )
+
+#
+# @admin.register(AdvertisementImage)
+# class AdvertisementImageAdmin(admin.ModelAdmin):
+#     pass
