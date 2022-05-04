63add692cf0d38bc53f2e473f8d4f1c4b60058e5
pemidi <rashidi.peyman@gmail.com>
2022-05-04 06:13:24 +0430

    refactor: user post advertisement

--- advertisement/forms.py
+++ advertisement/forms.py
@@ -1,9 +1,17 @@
 from django import forms
 
-from advertisement.models import Advertisement
+from advertisement.models import Advertisement, AdvertisementImage
+from category.models import Category
+from location.models import Location
 
 
-class PostAdvertisementForm(forms.ModelForm):
-    class Meta:
-        model = Advertisement
-        fields = ['title', 'description', 'price', 'location', 'category']
+class PostAdvertisementForm(forms.Form):
+    title = forms.CharField()
+    description = forms.CharField(widget=forms.Textarea)
+    price = forms.IntegerField()
+    location = forms.ModelChoiceField(queryset=Location.objects.all())
+    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    images = forms.ImageField()
+
+    def clean(self):
+        return self.cleaned_data
--- advertisement/views.py
+++ advertisement/views.py
@@ -8,8 +8,13 @@ class PostAdvertisementView(FormView):
     form_class = PostAdvertisementForm
     success_url = '/'
 
+
     def form_valid(self, form):
+        """
+        Get User from request
+        """
         instance = form.save(commit=False)
         instance.user = self.request.user
         instance.save()
         return super().form_valid(form)
+
