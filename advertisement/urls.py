e3a0271a3b15deea361d6b2ddb6b1da1ba8598e2
pemidi <rashidi.peyman@gmail.com>
2022-05-11 07:41:18 +0430

    change str to slug

--- advertisement/urls.py
+++ advertisement/urls.py
@@ -5,5 +5,5 @@ from advertisement.views import PostAdvertisementView, AdvertisementDetailView, 
 urlpatterns = [
     path('postadvertisement', PostAdvertisementView.as_view(), name='post-advertisement'),
     path('<pk>/', AdvertisementDetailView.as_view(), name='advertisement-detail'),
-    path('<str:city>', AdvertisementListView.as_view(), name='advertisement-list')
+    path('<slug:city>', AdvertisementListView.as_view(), name='advertisement-list'),
 ]
