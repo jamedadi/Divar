eb910e49518df14160e5d23f62209b21177ddd7d
pemidi <rashidi.peyman@gmail.com>
2022-05-11 08:17:51 +0430

    change name of AdvertisementCityCategoryListView url

--- advertisement/urls.py
+++ advertisement/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('postadvertisement', PostAdvertisementView.as_view(), name='post-advertisement'),
     path('<pk>/', AdvertisementDetailView.as_view(), name='advertisement-detail'),
     path('<slug:city>', AdvertisementListView.as_view(), name='advertisement-list'),
-    path('<slug:city>/<slug:category>', AdvertisementCityCategoryListView.as_view(), name='advertisement-list-category')
+    path('<slug:city>/<slug:category>', AdvertisementCityCategoryListView.as_view(),
+         name='advertisement-list-city-category')
 ]
