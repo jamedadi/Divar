b67cafe9e2934e25710c55bfeffc6fe8ba9dcf28
pemidi <rashidi.peyman@gmail.com>
2022-06-04 16:56:24 +0430

    fix: fix advetisement and account profile routing

--- advertisement/urls.py
+++ advertisement/urls.py
@@ -5,8 +5,8 @@ from advertisement.views import PostAdvertisementView, AdvertisementDetailView, 
 
 urlpatterns = [
     path('s/<slug:city>/', AdvertisementCityListView.as_view(), name='advertisement-list'),
-    path('s/<slug:city>/<slug:category>', AdvertisementCityCategoryListView.as_view(),
+    path('s/<slug:city>/<slug:category>/', AdvertisementCityCategoryListView.as_view(),
          name='advertisement-list-city-category'),
-    path('<pk>', AdvertisementDetailView.as_view(), name='advertisement-detail'),
-    path('postadvertisement', PostAdvertisementView.as_view(), name='post-advertisement'),
+    path('<pk>/', AdvertisementDetailView.as_view(), name='advertisement-detail'),
+    path('postadvertisement/', PostAdvertisementView.as_view(), name='post-advertisement'),
 ]
