056cc3dca1ffbff35f48bb08f06b79d9219c5305
pemidi <rashidi.peyman@gmail.com>
2022-06-04 19:42:51 +0430

    feat: add package models, add package modeladmin

--- package/admin.py
+++ package/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import register
 
-# Register your models here.
+from package.models import Package
+
+
+@register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'days', 'is_enable']
+    list_filter = ['is_enable']
+    search_fields = ['title']
