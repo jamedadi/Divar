efeae72adc22cbd11186a9539d8fa800f815f79c
pemidi <rashidi.peyman@gmail.com>
2022-06-07 04:59:42 +0430

    fix: change package url

--- package/urls.py
+++ package/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from package.views import PackageView
 
 urlpatterns = [
-    path('package/<int:package_pk>/<int:advertisement_pk>/', PackageView.as_view(), name='package-detail'),
+    path('<int:package_pk>/<int:advertisement_pk>/', PackageView.as_view(), name='package-detail'),
 ]
