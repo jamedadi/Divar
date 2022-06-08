abbd1fafede519729fdd94a8b4d1765b419cf71b
pemidi <rashidi.peyman@gmail.com>
2022-06-09 03:43:18 +0430

    add is_belong_user

--- package/views.py
+++ package/views.py
@@ -13,6 +13,8 @@ class PackageView(LoginRequiredMixin, View):
     template_name = 'package/package_advertisement.html'
 
     def get(self, request, package_pk, advertisement_pk):
+        if not Advertisement.is_belong_user(request.user, advertisement_pk):
+            raise Http404
         package = Package.objects.get(pk=package_pk)
         advertisement = Advertisement.objects.get(pk=advertisement_pk)
         return render(request, self.template_name, {'package': package, 'advertisement': advertisement})
