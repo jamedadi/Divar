f889a65464d8d28ec30413fbff3fa01a7f43be41
pemidi <rashidi.peyman@gmail.com>
2022-06-19 21:50:21 +0430

    add: add signals to the project

--- purchase/apps.py
+++ purchase/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PurchaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'purchase'
+
+    def ready(self):
+        import purchase.signals
